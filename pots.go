6e0f1ee94b53798f21b555cf4f68d77a93053d8e
Tristan Colgate <tcolgate@gmail.com>
2025-07-06 10:46:54 +0100

    move pot related code

--- pots.go
+++ pots.go
@@ -1,5 +1,33 @@
 package main
 
+const (
+	conPotUpdated = 1 << iota
+	cyanPotUpdated
+	magentaPotUpdated
+	yellowPotUpdated
+)
+
+type potUpdate struct {
+	vals    [4]uint16
+	updated uint8
+}
+
+func potChanged(o, n uint16) bool {
+	// This gives us 128 valid pot positions
+	minDiff := uint16(128)
+
+	if o > n {
+		return (minDiff < (o - n))
+	}
+
+	if o < n {
+		return (minDiff < (n - o))
+	}
+
+	return false
+}
+
+
 type potMgr struct {
 	lastConV    uint16
 	lastCyanV   uint16
